6f49798bc23fdf7b2b1177bcd575a764bfae41af
Mason Na <bohemianmoon5@gmail.com>
2023-08-13 21:29:43 +0900

    add code

--- 3_Main_Character.py
+++ 3_Main_Character.py
@@ -14,6 +14,17 @@ pygame.display.set_caption("Mason Game")  # 게임 이름
 background = pygame.image.load(
     "/Users/masonna/mini_game/grey_background.jpg")
 
+# 캐릭터(스프라이트) 불러오기
+character = pygame.image.load(
+    "/Users/masonna/mini_game/character.png")
+character_size = character.get_rect().size  # 이미지의 크기를 구해옴
+character_width = character_size[0]  # 캐릭터의 가로 크기
+character_height = character_size[1]  # 캐릭터의 세로 크기
+character_x_pos = (screen_width / 2) - (character_width /
+                                        2)  # 화면 가로의 절반 크기에 해당하는 곳에 위치(가로)
+# 화면 세로 크기 가장 아래에 해당하는 곳에 위치(세로)
+character_y_pos = screen_height - character_height
+
 # 이벤트 루프
 running = True  # 게임이 진행 중인가?
 while running:
@@ -23,6 +34,8 @@ while running:
 
     #screen.fill((0, 0, 255))
     screen.blit(background, (0, 0))  # 배경 그리기
+
+    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
 
     pygame.display.update()  # 게임화면을 다시 그리기!
 
